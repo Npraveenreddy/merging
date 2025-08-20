78ae267d0f6fe04d2cc8ca1848f3f044c1b1604e
PraveenReddy Narayana <narayana.praveen143@gmail.com>
2025-08-20 15:40:50 +0530

    Add resource-names.ts file with resource names

---
+++ lib/resource-names.ts
@@ -0,0 +1,6 @@
+export const resourceNames = {
+  bucketName: 'athena-query-results-bucket-demo-001',
+  workGroupName: 'athena-demo-workgroup',
+  dbName: 'athena_demo_db',
+  tableName: 'athena_demo_table',
+};
